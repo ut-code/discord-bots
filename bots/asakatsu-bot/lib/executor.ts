a143f63930929fa6489dbfcc1229ad780fb39644
aster <137767097+aster-void@users.noreply.github.com>
2026-01-09 19:09:41 +0900

    bots/asakatsu-bot: fix executor

--- bots/asakatsu-bot/lib/executor.ts
+++ bots/asakatsu-bot/lib/executor.ts
@@ -13,26 +13,26 @@ export async function execWithContext(fn: (c: Context) => Promise<void>) {
 		partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 	});
 
-	const channel = await client.channels.fetch(env.CHANNEL_ID);
-	if (!channel?.isTextBased()) {
-		console.error(
-			"指定チャンネルが見つからないか、テキストチャンネルではありません",
-		);
-		return;
-	}
-	if (!channel.isSendable()) {
-		console.error("チャンネルはメッセージ送信不可能です");
-		return;
-	}
-
-	const ctx: Context = {
-		env,
-		channel,
-		client,
-	};
-
 	client.once("ready", async () => {
 		try {
+			const channel = await client.channels.fetch(env.CHANNEL_ID);
+			if (!channel?.isTextBased()) {
+				console.error(
+					"指定チャンネルが見つからないか、テキストチャンネルではありません",
+				);
+				return;
+			}
+			if (!channel.isSendable()) {
+				console.error("チャンネルはメッセージ送信不可能です");
+				return;
+			}
+
+			const ctx: Context = {
+				env,
+				channel,
+				client,
+			};
+
 			await fn(ctx);
 		} finally {
 			client.destroy();
